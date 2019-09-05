501c50b0f837116dd840014b4e15e5e123414322
Jimmy Ghelani <jghelani@sunwing.ca>
2019-09-05 15:52:34 -0400

    indent files

--- wordpress/wp-content/plugins/scripts/scripts.php
+++ wordpress/wp-content/plugins/scripts/scripts.php
@@ -12,46 +12,44 @@ add_action('admin_menu', 'scripts_admin_menu_option');
 
 function scripts_page(){
 
-    if(array_key_exists('scripts_update', $_POST)) {
-        update_option('header_script', $_POST['headerscript']);
-        update_option('footer_script', $_POST['footerscript']);
+	if(array_key_exists('scripts_update', $_POST)) {
+		update_option('header_script', $_POST['headerscript']);
+		update_option('footer_script', $_POST['footerscript']);
 
-        ?>
-        <div id="setting-error-settings-updated" class="updated settings-error notice is-sismissible"><strong>Settings have been saved</strong></div>
-        <?php
-    }
+		?>
+		<div id="setting-error-settings-updated" class="updated settings-error notice is-sismissible"><strong>Settings have been saved</strong></div>
+		<?php
+	}
 
-    $header_scripts = get_option('header_script', 'none');
-    $footer_scripts = get_option('footer_script', 'none');
+	$header_scripts = get_option('header_script', 'none');
+	$footer_scripts = get_option('footer_script', 'none');
 	?>
 
-    <div class="wrap">
-        <h2>Header and Footer Scripts</h2>
-        <form method="post" action="">
-            <label for="headerscript">Header Scripts</label>
-            <textarea class="large-text" name="headerscript" id="headerscript">
-                <?php print $header_scripts ?>
-            </textarea>
-            <label for="footerscript">Header Scripts</label>
-            <textarea class="large-text" name="footerscript" id="footerscript">
-                <?php print $footer_scripts ?>
-            </textarea>
-            <input type="submit" name="scripts_update" value="UPDATE SCRIPTS" class="button button-primary">
-        </form>
-    </div>
-    <?php
+	<div class="wrap">
+		<h2>Header and Footer Scripts</h2>
+		<form method="post" action="">
+			<label for="headerscript">Header Scripts</label>
+			<textarea class="large-text" name="headerscript" id="headerscript"><?php print $header_scripts ?></textarea>
+			<label for="footerscript">Header Scripts</label>
+			<textarea class="large-text" name="footerscript" id="footerscript">
+				<?php print $footer_scripts ?>
+			</textarea>
+			<input type="submit" name="scripts_update" value="UPDATE SCRIPTS" class="button button-primary">
+		</form>
+	</div>
+	<?php
 }
 
 function display_header_scripts(){
-    $header_scripts = get_option('header_script', 'none');
+	$header_scripts = get_option('header_script', 'none');
 
-    print $header_scripts;
+	print $header_scripts;
 }
 add_action('wp_head', 'display_header_scripts');
 
-    function display_footer_scripts(){
-	    $footer_scripts = get_option('footer_script', 'none');
+	function display_footer_scripts(){
+		$footer_scripts = get_option('footer_script', 'none');
 
-	    print $footer_scripts;
-    }
+		print $footer_scripts;
+	}
 add_action('wp_footer', 'display_footer_scripts');
